2219f881178db88211fd13861d8993ca207c44f5
¨Lautaro <lautaro.fernandez@gm2dev.com>
2024-10-06 20:11:06 -0300

    feat a classlist for each pokemon

--- pokeapi.v2/pokeapi.js
+++ pokeapi.v2/pokeapi.js
@@ -1,53 +1,54 @@
-let container = document.querySelector(".pokemons")
+let container = document.querySelector(".pokemons");
 
 const pokeUrl = "https://pokeapi.co/api/v2/pokemon/";
 let = nextLink = "";
 let = prevLink = "";
 
 const prev = () => {
-    getPokemons(prevLink);
-}
+  getPokemons(prevLink);
+};
 const next = () => {
-    getPokemons(nextLink);
-}
+  getPokemons(nextLink);
+};
 const getPokemons = (url) => {
-    fetch(url)
-        .then(response => response.json())
-        .then(responseJson => {
-            console.log(responseJson);
-            nextLink =responseJson.next;
-            prevLink = responseJson.previous;
-            showPokemons(responseJson.results);
-        })
-}
+  fetch(url)
+    .then((response) => response.json())
+    .then((responseJson) => {
+      console.log(responseJson);
+      nextLink = responseJson.next;
+      prevLink = responseJson.previous;
+      showPokemons(responseJson.results);
+    });
+};
 const showPokemons = (array) => {
-    clearContainer();
-    array.map(item => {
-        fetch(item.url)
-        .then(response => response.json())
-        .then(data => {
-            console.log(data);
-            loadCard(data);
-        })
-    })
-}
+  clearContainer();
+  array.map((item) => {
+    fetch(item.url)
+      .then((response) => response.json())
+      .then((data) => {
+        console.log(data);
+        loadCard(data);
+      });
+  });
+};
 
 const loadCard = (data) => {
-    const image = data.sprites.other["official-artwork"].front_default;
-    const name = data.name;
-    const types = data.types;
-    const type = types.map((types) => {
+  const image = data.sprites.other["official-artwork"].front_default;
+  const name = data.name;
+  const types = data.types;
+  const type = types.map((types) => {
     return types.type.name;
   });
 
-    let card = document.createElement("div");
-    let content = `
+  let card = document.createElement("div");
+  card.classList.add("pokemon");
+  let content = `
     <img src="${image}" alt="${name}">
     <p>${name}</p>
     <p>${type}</p>
     `;
-    card.innerHTML = content;
-    container.appendChild(card);
-}
-const clearContainer = () => container.innerHTML = ""
-getPokemons(`${pokeUrl}?offset=0&limit=25`)
+  card.innerHTML = content;
+  container.appendChild(card);
+};
+const clearContainer = () => (container.innerHTML = "");
+getPokemons(`${pokeUrl}?offset=0&limit=25`);
